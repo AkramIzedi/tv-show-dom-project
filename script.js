06dde1f39eb21c4542e5cbc8adc5351637ab34dd
akramizedi <afsoonewss@gmail.com>
2023-05-05 23:12:10 +0100

    finished step 1 , 2

--- script.js
+++ script.js
@@ -1,12 +1,61 @@
 //You can edit ALL of the code here
+const lable = document.getElementById("lable");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
+  let output ="";
+  episodeList.forEach((episode) => {
+       
+    let seasonNum = episode.season.toString().padStart(2, "0");
+     let episodeNum = episode.number.toString().padStart(2, "0");
+     output +=`
+     <div style="background-color:white; width:270px;">
+     <h1 style="color:black;">${episode.name} - S${seasonNum}E${episodeNum}</h1>
+     <img src="${episode.image.medium}"></img>
+     <div><p>${episode.summary}</p></div>
+     </div>
+     `;
+   
+    
+  });
+
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = output;
+  let episodeNumber = document.createElement("div");
+  episodeNumber.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.appendChild(episodeNumber);
+  
+  return output;
+ 
 }
 
-window.onload = setup;
+window.onload = setup();
+
+
+
+
+
+// 
+
+const input = document.querySelector("#search-bar");
+input.addEventListener("keyup" , function (e) {
+  let result = "";
+  const inputValue = e.target.value;
+  const allEpisodes = getAllEpisodes();
+  let inputValueInsensitive = new RegExp(inputValue,"i");
+  let selectedEpisodes = allEpisodes.filter((episode) =>{
+    if (
+      episode.name.match(inputValueInsensitive) !== null ||
+      episode.summary.match(inputValueInsensitive) !== null
+    ) {
+      return episode;
+    }
+  });
+console.log(selectedEpisodes);
+  result = makePageForEpisodes(selectedEpisodes);
+  lable.innerText = `Displaying ${selectedEpisodes.length}/${allEpisodes.length} episodes`;
+});
